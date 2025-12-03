cf0616a89a841674bf0a52c91197eb9bbb9bdda1
sanjanaspals1106 <sanjanaspals1106@gmail.com>
2025-12-03 12:18:16 +0530

    Create utils.py

---
+++ mini-projects/text-utilities/utils.py
@@ -0,0 +1,17 @@
+def reverse_text(text):
+    pass
+
+def count_vowels(text):
+    pass
+
+def is_palindrome(text):
+    pass
+
+def char_frequency(text):
+    pass
+
+def remove_extra_spaces(text):
+    pass
+
+def find_substring(text, sub):
+    pass
