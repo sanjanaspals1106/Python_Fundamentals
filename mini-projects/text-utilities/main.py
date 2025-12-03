96c59f78307cc6f569c7d2400e9971964458ffc3
sanjanaspals1106 <sanjanaspals1106@gmail.com>
2025-12-03 12:17:36 +0530

    Create main.py

---
+++ mini-projects/text-utilities/main.py
@@ -0,0 +1,21 @@
+# main.py – structure only
+
+from utils import (
+    reverse_text,
+    count_vowels,
+    is_palindrome,
+    char_frequency,
+    remove_extra_spaces,
+    find_substring,
+)
+
+def main():
+    # Print menu
+    # Loop to take user input
+    # Based on choice, call the corresponding function from utils.py
+    # Take input text from user when needed
+    # Print the results
+    pass   # remove when you write logic
+
+if __name__ == "__main__":
+    main()
